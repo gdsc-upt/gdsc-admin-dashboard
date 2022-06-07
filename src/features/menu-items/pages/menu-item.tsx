ed3f2239e4fb048f42f4f15594759a01e6bb3c37
Dan Percic <danpercic86@gmail.com>
2022-06-07 16:07:48 +0300

    update menu items

--- src/features/menu-items/pages/menu-item.tsx
+++
@@ -1,9 +0,0 @@
-import { MenuItem } from '../models/menu-item';
-
-export interface MenuItemProps {
-  readonly menuItem: MenuItem;
-}
-
-export function MenuItem(props: MenuItemProps) {
-  const { menuItem } = props;
-}
