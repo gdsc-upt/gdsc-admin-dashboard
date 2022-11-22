103ba56d22a8401a15caf0d3dd439b4975263dc5
Madalin Pothora <unicornshovel@gmail.com>
2022-11-22 23:02:16 +0200

    bruh

--- src/features/redirects/routes.tsx
+++ src/features/redirects/routes.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { RouteObject } from "react-router-dom";
 import { REDIRECTS_URLS } from "./urls";
 import { IfLoggedIn } from "../auth/services/auth-context";
+import { RedirectsPage } from "./pages/redirects-page";
 
 export function RedirectRoutes(): RouteObject[] {
   return [
     {
       path: REDIRECTS_URLS.redirects,
-      element:,
+      element: (
+        <IfLoggedIn>
+          <RedirectsPage />
+        </IfLoggedIn>
+      ),
     },
   ];
 }
