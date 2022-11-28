de5bf2843665c2908eaee0b05a36fce0cc281853
Dan Percic <danpercic86@gmail.com>
2022-11-28 22:35:51 +0200

    update login page

--- src/features/auth/pages/login.tsx
+++ src/features/auth/pages/login.tsx
@@ -1,9 +1,26 @@
-import React from "react";
-import { Box } from "@mui/material";
-import { Logo } from "../../../components/logo";
-import { LoginForm } from "../components/login-form";
+import React, { useState } from "react";
 
-export function LoginPage() {
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { useLocation } from "react-router-dom";
+import { AxiosError } from "axios";
+import { LoginForm } from "../components/login-form";
+import { Logo } from "../../../components/logo";
+import { LoginRequest } from "../models/login.request";
+import { URLS } from "../../../helpers/constants";
+import { useAuth } from "../components/auth-context";
+import { useRouting } from "../../../routing";
+
+export function LoginPagess() {
   return (
     <>
       <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
@@ -11,5 +28,126 @@ export function LoginPage() {
       </Box>
       <LoginForm />
     </>
+  );
+}
+
+function Copyright(props: any) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>
+      {" "}
+      {new Date().getFullYear()}
+      .
+    </Typography>
+  );
+}
+
+export function LoginPage() {
+  const [error, setError] = useState<string | undefined>(undefined);
+  const { routeTo } = useRouting();
+  const auth = useAuth();
+  const location = useLocation();
+
+  const handleError = (apiError: AxiosError) => {
+    if (apiError.response?.status === 401) {
+      return setError("Invalid username or password");
+    }
+
+    return setError("Something went wrong :(");
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    return tryLogin({
+      username: data.get("email") as string,
+      password: data.get("password") as string,
+    });
+  };
+
+  const tryLogin = async (loginRequest: LoginRequest) => {
+    const from = location.state?.from?.pathname || URLS.dashboard;
+    auth.signIn(loginRequest, () => routeTo(from), handleError);
+  };
+
+  return (
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(../../../assets/images/gdsc-logo.png)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: t => (t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900]),
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="/register" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  No account? Sign Up
+                </Link>
+              </Grid>
+            </Grid>
+            <Copyright sx={{ mt: 5 }} />
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
